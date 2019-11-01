d40325599fc4c37864584660d7eaa82f0ede25e5
Ozge <ozgey99@gmail.com>
2019-11-01 19:34:12 +0100

    2 new files added

---
+++ 1. Northwind API/Models/ProductForCreation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1._Northwind_API.Models
+{
+    public class ProductForCreation
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string Category { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public int UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+    }
+}
---
+++ 1. Northwind API/Profiles/ProductProfile.cs
@@ -0,0 +1,20 @@
+﻿using _0._Models;
+using _1._Northwind_API.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._Northwind_API.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDto>();
+            CreateMap<ProductForCreation, Product>();
+        }
+    }
+}
