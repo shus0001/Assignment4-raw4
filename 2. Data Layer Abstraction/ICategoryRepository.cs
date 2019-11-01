d17eb7c1aafb6c2eb0ebf58a40dc6622cddde45d
Ozge <ozgey99@gmail.com>
2019-11-01 18:48:32 +0100

    removed paging

--- 2. Data Layer Abstraction/ICategoryRepository.cs
+++ 2. Data Layer Abstraction/ICategoryRepository.cs
@@ -6,7 +6,6 @@ namespace _2._Data_Layer_Abstraction
 {
     public interface ICategoryRepository : IGetSingleRepository<Category>, IGetAllRepository<Category>
     {
-        IList<Category> GetCategories(PagingAttributes pagingAttributes);
         int NumberOfCategories();
         Category Create(string name, string description);
         bool Update(int id, string name, string description);
