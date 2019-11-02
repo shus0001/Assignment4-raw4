ae59aa4348fb54dead611c937418c6e1afd8ccc3
Manish Shrestha <manish.05shrestha@gmail.com>
2019-11-02 09:23:29 +0100

    Products 1st test pass and cleanup

--- 1. Northwind API/Controllers/ProductsController.cs
+++ 1. Northwind API/Controllers/ProductsController.cs
@@ -3,41 +3,30 @@ using _1._Northwind_API.Models;
 using _2._Data_Layer_Abstraction;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace _1._Northwind_API.Controllers
 {
     [ApiController]
     [Route("api/products")]
-    public class ProductsController
+    public class ProductsController : ControllerBase
     {
         private IProductRepository productRepository;
         private IMapper mapper;
-
         public ProductsController(IProductRepository productRepository, IMapper mapper)
         {
             this.productRepository = productRepository;
             this.mapper = mapper;
         }
 
-        [HttpGet(Name = nameof(GetProducts))]
-        public ActionResult GetProducts()
-        {
-            var products = productRepository.GetAll();
-
-            return Ok(products);
-        }
-
         [HttpGet("{productId}", Name = nameof(GetProduct))]
         public ActionResult GetProduct(int productId)
         {
-            var category = productRepository.GetById(productId);
-            if (category == null)
+            var product = productRepository.GetById(productId);
+            if (product == null)
             {
                 return NotFound();
             }
-            return Ok(CreateProductDto(category));
+            return Ok(CreateProductDto(product));
         }
 
         ///////////////////
@@ -50,14 +39,9 @@ namespace _1._Northwind_API.Controllers
         {
             var dto = mapper.Map<ProductDto>(product);
             dto.Link = Url.Link(
-                nameof(GetProduct),
-                new { productId = product.Id });
+                    nameof(GetProduct),
+                    new { productId = product.Id });
             return dto;
-        }
-
-        private IEnumerable<ProductDto> CreateResult(IEnumerable<Product> products)
-        {
-            return products.Select(p => CreateProductDto(p));
         }
     }
 }
--- 1. Northwind API/Startup.cs
+++ 1. Northwind API/Startup.cs
@@ -16,15 +16,14 @@ namespace _1._Northwind_API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var northwindContext = new NorthwindContext();
             services.AddControllers();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-            services.AddSingleton<ICategoryRepository>(provider =>
-            {
-                var northwindContext = new NorthwindContext();
-                return new CategoryRepository(northwindContext);
-            });
+            services.AddSingleton<ICategoryRepository>(provider => new CategoryRepository(northwindContext));
+
+            services.AddSingleton<IProductRepository>(provider => new ProductRepository(northwindContext));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
